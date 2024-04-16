02d095d353f49928bbde6fd6ab472bd822191c1b
Federico G. Ramos <federicoramos@me.com>
2024-04-16 07:29:04 +0200

    Initial Commit

---
+++ NetworkStatus/NetworkStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkStatusApp.swift
+//  NetworkStatus
+//
+//  Created by Federico G. Ramos on 16.04.24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
